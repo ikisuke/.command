ef646126978b6048548caa3fbc62d8f0a9e6d6aa
ikisuke <46296566+ikisuke@users.noreply.github.com>
2024-12-23 11:11:42 +0900

    Update init.sh

--- idea/init.sh
+++ idea/init.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
+# memo_vault が未設定の場合は赤色でエラーメッセージを出力して終了
+if [ -z "$memo_vault" ]; then
+      echo -e "\e[31mError: memo_vault is not set.\e[0m"
+      exit 1
+fi
+
 # 現在のディレクトリ名を取得
 current_dir=$(basename "$PWD")
 
-# TODO: 目的のディレクトリパスを受け付ける必要がありそう
-# ex, zsh env file, or command args
-# init "/path/to/your/folder/"
-
-# 目的のディレクトリパスを設定
-target_dir="/path/to/your/folder/${current_dir}"
+# 目的のディレクトリパスを設定（memo_vault 内に current_dir と同名のディレクトリを作成）
+target_dir="$memo_vault/$current_dir"
+echo "The value of memo_vault is: $memo_vault"
 
 # 目的のディレクトリが存在しない場合は作成
 if [ ! -d "$target_dir" ]; then
-  mkdir -p "$target_dir"
-  echo "Created directory: $target_dir"
+      mkdir -p "$target_dir"
+      echo "Created directory: $target_dir"
 else
-  echo "Directory already exists: $target_dir"
+      echo "Directory already exists: $target_dir"
 fi
 
-# シンボリックリンクを作成
+# シンボリックリンクを作成（.memo.local -> target_dir）
 ln -sfn "$target_dir" .memo.local
 echo "Created symlink: .memo.local -> $target_dir"
